4c9925183a2334eafa099b59c35911e89ca8e963
Eric Patterson <epatts33@gmail.com>
2023-12-29 11:21:55 -0600

    Initial Commit

---
+++ Dart01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dart01
+//
+//  Created by Eric Patterson on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dart01/Dart01App.swift
@@ -0,0 +1,17 @@
+//
+//  Dart01App.swift
+//  Dart01
+//
+//  Created by Eric Patterson on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Dart01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
