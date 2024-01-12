812274e4d829d2d9de514fbf418a1a2c6866001d
Eric Patterson <epatts33@gmail.com>
2024-01-12 10:56:46 -0600

    Small styling change to prevent number pads from moving when user gets down to a checkout

--- Dart01/View/ScoreView.swift
+++ Dart01/View/ScoreView.swift
@@ -12,7 +12,7 @@ struct ScoreView: View {
     
     var body: some View {
         Text("\(score)")
-            .font(Theme.Fonts.ralewaySemiBold(CheckoutNumbers.shared.isCheckoutNumber(score) ? 150 : 180, .largeTitle))
+            .font(Theme.Fonts.ralewaySemiBold(CheckoutNumbers.shared.isCheckoutNumber(score) ? 137 : 180, .largeTitle))
             .frame(maxWidth: .infinity)
             .contentTransition(.numericText(countsDown: true))
             .padding(.top, CheckoutNumbers.shared.isCheckoutNumber(score) ? -30 : -40)
